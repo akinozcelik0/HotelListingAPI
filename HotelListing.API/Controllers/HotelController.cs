632120b01cf28e204e0d56ad6efd4eb5223ee742
Akın Özçelik <110735711+akinozcelik0@users.noreply.github.com>
2023-02-14 00:40:13 +0300

    Hotel Dto and Repository implemented. HotelController Refactored.

---
+++ HotelListing.API/Controllers/HotelController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelListing.API.Data;
+using HotelListing.API.Contracts;
+using AutoMapper;
+using HotelListing.API.Models.Hotel;
+
+namespace HotelListing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly IHotelRepository _hotelRepository;
+        private readonly IMapper _mapper;
+
+        public HotelController(IHotelRepository hotelRepository, IMapper mapper)
+        {
+            _hotelRepository = hotelRepository;
+            _mapper = mapper;
+        }
+
+        // GET: api/Hotel
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HotelDto>>> GetHotels()
+        {
+            var hotels = await _hotelRepository.GetAllAsync();
+            return Ok(_mapper.Map<List<HotelDto>>(hotels));
+        }
+
+        // GET: api/Hotel/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<HotelDto>> GetHotel(int id)
+        {
+            var hotel = await _hotelRepository.GetAsync(id);
+
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<HotelDto>(hotel));
+        }
+
+        // PUT: api/Hotel/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHotel(int id, HotelDto hotelDto)
+        {
+            if (id != hotelDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var hotel = await _hotelRepository.GetAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(hotelDto, hotel);
+
+            try
+            {
+                await _hotelRepository.UpdateAsync(hotel);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await HotelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Hotel
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Hotel>> PostHotel(CreateHotelDto hotelDto)
+        {
+            var hotel = _mapper.Map<Hotel>(hotelDto);
+            await _hotelRepository.AddAsync(hotel);
+
+            return CreatedAtAction("GetHotel", new { id = hotel.Id }, hotel);
+        }
+
+        // DELETE: api/Hotel/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHotel(int id)
+        {
+            var hotel = await _hotelRepository.GetAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            await _hotelRepository.DeleteAsync(id);
+
+            return NoContent();
+        }
+
+        private async Task<bool> HotelExists(int id)
+        {
+            return await _hotelRepository.Exists(id);    
+        }
+    }
+}
---
+++ HotelListing.API/Models/Hotel/BaseHotelDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.Models.Hotel
+{
+    public abstract class BaseHotelDto
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Name { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Address { get; set; }
+
+        public double? Rating { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [Range(1,int.MaxValue)]
+        public int CountryId { get; set; }
+
+    }
+}
